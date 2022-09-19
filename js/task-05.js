da27921d8f43016d4fb5d903d35d6f643e101534
AdrianG24 <adriangolodisin@gmail.com>
2022-09-19 12:37:34 +0300

    all main tasks

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,8 @@ const newName = document.querySelector("#name-output");
 
 oldName.oninput = function () {
   newName.innerHTML = oldName.value;
+  if (newName.innerHTML === "") {
+    newName.innerHTML = "Anonymous";
+  }
 };
 console.log(oldName.value);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.getElementById("change-color");
+console.log(btn);
+
+btn.addEventListener("click", function onClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+});
